84022131fd2791e372f4d621926bab67791fdd46
Bo0tzz <btdewilligen@gmail.com>
2018-11-18 12:27:44 +0100

    Clean up test_detect

--- dnsdetect/test_detect.py
+++ dnsdetect/test_detect.py
@@ -5,34 +5,38 @@ import unittest
 
 class TestDetect(unittest.TestCase):
 
+    def setUp(self):
+        self.under_test = detect.Detect()
+
     @parameterized.expand([
         ['assets.funnygames.at', 'd.gcdn.co.'],
         ['edition.i.cdn.cnn.com', 'turner-tls.map.fastly.net.'],
         ['cdn.cnn.com', 'e12596.dscj.akamaiedge.net.']
     ])
     def test_cname_chain(self, base_url, expected):
-        root_cname = detect.find_root_cname(base_url)
+        root_cname = self.under_test.find_root_cname(base_url)
         self.assertEqual(expected, root_cname)
 
     @parameterized.expand([
         ['54.230.129.171', 'server-54-230-129-171.ams50.r.cloudfront.net']  # TODO: Find more relevant test cases
     ])
     def test_reverse_dns(self, ip, expected):
-        url = detect.reverse_dns(ip)
+        url = self.under_test.reverse_dns(ip)
         self.assertEqual(expected, url)
 
     @parameterized.expand([
-        ['www.funnygames.at', True],
-        ['static.ah.nl', True],
-        ['images.nrc.nl', True],
-        ['www.cloudflare.com', True],
-        ['www.warpcache.com', False],
-        ['www.google.com', False],
-        ['www.tweakers.net', False]
-    ])
+        ['funnygames.at.', True],
+        ['static.ah.nl.', True],
+        ['images.nrc.nl.', True],
+        ['cloudflare.com.', True],
+        ['warpcache.com.', False],
+        ['google.com.', False],
+        ['tweakers.net.', False]
+    ])  # This test is having problems on my system - DNS records won't resolve properly.
     def test_detect_cloudflare(self, url, is_cloudflare):
-        res = detect.detect_cloudflare(url)
+        res = self.under_test.detect_cloudflare(url)
         self.assertEqual(is_cloudflare, res)
+
 
 if __name__ == '__main__':
     unittest.main()
