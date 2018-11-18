10e72233a1dcf4684c9db4621503bc5f3bc2f723
Bo0tzz <btdewilligen@gmail.com>
2018-11-18 12:28:15 +0100

    Turn detect.py into a class, add cloudflare detection

--- README.md
+++ README.md
@@ -4,3 +4,6 @@ A python service that will detect the CDNs used by a website
 
 ### Issues encountered
 * If assets are hosted on a different domain, should this be detected?
+* During development I am having some issues with DNS - records that certainly exist won't be resolved,
+  eg: `dns.resolver.NoAnswer: The DNS response does not contain an answer to the question: funnygames.at. IN A`
+  This makes testing problematic.
--- dnsdetect/detect.py
+++ dnsdetect/detect.py
@@ -1,22 +1,42 @@
 import socket
+import requests
 
 from dns import resolver
 from dns.resolver import NoAnswer
+import ipaddress
 
+class Detect:
 
-def find_root_cname(url: str):
-    loop = True
-    while loop:
-        try:
-            url = resolver.query(url, 'CNAME')[0].to_text()
-        except NoAnswer:  # We have reached the bottom of the CNAME chain
-            loop = False
-    return url
+    def __init__(self):
+        self.cloudflare_ranges = []
+        r = requests.get('https://cloudflare.com/ips-v4')
+        for l in r.text.split('\n'):
+            if l != '':
+                self.cloudflare_ranges.append(l)
 
+    def detect_cloudflare(self, url: str):
+        url = self.find_root_cname(url)
+        ip = resolver.query(url, 'A')[0].to_text()
+        is_cloudflare = False
+        ip_addr = ipaddress.ip_address(ip)
+        print(self.cloudflare_ranges)
+        for r in self.cloudflare_ranges:
+            if ip_addr in ipaddress.ip_network(r):
+                is_cloudflare = True
+        return is_cloudflare
 
-def reverse_dns(ip: str):
-    try:
-        url = socket.gethostbyaddr(ip)[0]
+    def find_root_cname(self, url: str):
+        loop = True
+        while loop:
+            try:
+                url = resolver.query(url, 'CNAME')[0].to_text()
+            except NoAnswer:  # We have reached the bottom of the CNAME chain
+                loop = False
         return url
-    except socket.herror:  # No result
-        return ''
+
+    def reverse_dns(self, ip: str):
+        try:
+            url = socket.gethostbyaddr(ip)[0]
+            return url
+        except socket.herror:  # No result
+            return ''
